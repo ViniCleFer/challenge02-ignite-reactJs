3ecfc72d7e46cebc5f1eb424c7ab42f5e913d759
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2022-01-29 01:50:19 -0300

    finish challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,35 @@
-export function SideBar() {
-  // Complete aqui
+import { GenreResponseProps } from '../App';
+
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreResponseProps[];
+  handleClickButton: (genreId: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}: SideBarProps) {
+  return (
+    <nav className='sidebar'>
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className='buttons-container'>
+        {genres.map((genre: GenreResponseProps) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
